e3b37aa78ba99b8deb6249d42b842d2996ec1951
Charanaraj N <goudacharanraj760@gmail.com>
2023-01-01 19:09:37 +0530

    added getNextDate func

--- palindrome.js
+++ palindrome.js
@@ -76,21 +76,55 @@ function isLeapYear(year){
 }
 
 function getNextDate(date){
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
 
+    const arrOfMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+
+    if(month === 2){
+        if(isLeapYear(year)){
+            if(day > 29){
+                day = 1;
+                month++;
+            }
+        } else {
+            if(day > 28){
+                day = 1; 
+                month++;
+            }
+        }
+    } else {
+        if(day > arrOfMonths[month - 1]){
+            day = 1;
+            month++
+        }
+    }
+
+    if(month > 12){
+        month = 1;
+        year++;
+    }
+
+    return {
+        day: day,
+        month: month,
+        year: year
+    }
 
 }
 
 function getNextPalindromeDate(date){
-    
+
 }
 
 var date = {
-    day: 2,
-    month: 2,
-    year: 2020
+    day: 31,  
+    month: 12,
+    year: 2021
 }
 
 
-console.log(checkPalindromeForAllDateFormats(date));
+console.log(getNextDate(date));
 
 
